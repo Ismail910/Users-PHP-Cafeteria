7ea03a8998e70b98beae81e8b21faee4913c5317
IbrahimSalahEldin <ibrahimsalah824@gmail.com>
2023-05-02 02:32:37 +0300

    add orders

--- App/Views/template/sidebar.php
+++ App/Views/template/sidebar.php
@@ -10,22 +10,25 @@
     </a>
     </li>
 
-    <li class="row g-0 active">
+    <!-- <li class="row g-0 active">
      <a href="<?php url('categories/index'); ?>">
      <i class="fa-solid fa-briefcase"></i> 
      <p>Categories</p>
     </a>
-    </li>
+    </li> -->
 
-    <li class="row g-0"><a href="<?php url('products/index'); ?>">
+    <!-- <li class="row g-0"><a href="<?php url('products/index'); ?>">
     <i class="fa-solid fa-cart-shopping"></i>
     <p>Products</p>
-    </a></li>
-    <li class="row g-0"><a href="<?php url('users/index'); ?>">
+    </a></li> -->
+
+    <!-- <li class="row g-0"><a href="<?php url('users/index'); ?>">
     <i class="fa fa-users"></i><p>Users</p></href=></li>
     <li class="row g-0"><a href="../checks/index">
     <i class="fa-solid fa-cart-shopping"></i>
-        <p>Checks</p></a></li>
+        <p>Checks</p></a></li> -->
+
+        
     <li class="row g-0"><a href=""> <i class="fa-solid fa-cart-shopping"></i><p>orders</p></a></li>
    </ul>
 
