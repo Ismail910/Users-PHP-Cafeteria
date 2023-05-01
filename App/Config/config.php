be4ab912b8a45bb37bbb98ad146c232e4f08b02a
Ismail910 <ismailmagdy566@gmail.com>
2023-05-01 23:21:58 +0300

    create project

---
+++ App/Config/config.php
@@ -0,0 +1,14 @@
+<?php 
+// define pase url for the project 
+
+define("BURL","http://cafeteria.local:700/");
+
+/**
+ * database configuration 
+ */
+
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASS","");
+define("DB_PORT",3306);
+define("DB_NAME","cafeteria");
---
+++ App/Views/template/sidebar.php
@@ -0,0 +1,32 @@
+
+<sidebar class="row g-0">
+    <div class="logo text-center"><h2>CAFFEE</h2></div>
+
+    <ul class="text-center">
+    <li class="row g-0">
+    <a href="">
+    <i class="fa fa-home"></i>
+    <p>Home</p>
+    </a>
+    </li>
+
+    <li class="row g-0 active">
+     <a href="<?php url('categories/index'); ?>">
+     <i class="fa-solid fa-briefcase"></i> 
+     <p>Categories</p>
+    </a>
+    </li>
+
+    <li class="row g-0"><a href="<?php url('products/index'); ?>">
+    <i class="fa-solid fa-cart-shopping"></i>
+    <p>Products</p>
+    </a></li>
+    <li class="row g-0"><a href="<?php url('users/index'); ?>">
+    <i class="fa fa-users"></i><p>Users</p></href=></li>
+    <li class="row g-0"><a href="../checks/index">
+    <i class="fa-solid fa-cart-shopping"></i>
+        <p>Checks</p></a></li>
+    <li class="row g-0"><a href=""> <i class="fa-solid fa-cart-shopping"></i><p>orders</p></a></li>
+   </ul>
+
+</sidebar>
