99c3b6ab2a87062a37d684a69744060b97e773ce
Ismail910 <ismailmagdy566@gmail.com>
2023-05-08 09:44:41 +0300

    marge

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -26,81 +26,158 @@ class HomeController extends Controller
         return $this->view('products/index', $data);
     }
 
-    public function addToCard($id)
+
+
+
+    public function cart($id,$quantity,$total)
     {
+                  
+        // var_dump($_SESSION['cart']);
+        // return;
 
-        $Product = $this->product->getProduct($id);
-       
-
-        session_start();
-        if (!isset($_SESSION['card'])) {
-            $_SESSION['card'] = array();
-        }
-
-
-        for ($i = 0; $i < count($_SESSION['card']); $i++) {
-            if ($Product[0]["name"] == $_SESSION['card'][$i]['name']) {
-                $_SESSION['card'][$i]['quantity'] += 1;
-                $_SESSION['card'][$i] = [
-                    'name' => $Product[0]["name"],
-                    'price' => $Product[0]["price"] * $_SESSION['cart'][$i]['quantity'],
-                    'quantity' => $_SESSION['cart'][$i]['quantity'],
-                    'id' => $Product[0]["id"]
-                ];
-                echo json_encode($_SESSION['card'][$i]);
-                return;
-            }
-        }
-
-        try {
-            $item = [
-                "name" => $Product[0]["name"],
-                "price" => $Product[0]["price"],
-                "quantity" => 1
-            ];
-            $_SESSION['card'] =  $item;
-            $data['success'] = "Product Have Been saved";
-            $result = [
-                'name' => $Product[0]["name"],
-                'price' => $Product[0]["price"],
-                'quantity' => 1,
-                'id' =>$Product[0]["id"],
+        $totalPrice=$total;
+        $pro=$this->product->getProduct($id);
+        if($pro)    
+        {
             
+            session_start();
+            // Check if the cart exists in the session
+            if (!isset($_SESSION['cart'])) {
+            // If the cart doesn't exist, create an empty array
+            $_SESSION['cart'] = array();
+            }
+            
+            $i=0;
+            
+            foreach ($_SESSION['cart'] as $result){
+                if($pro[0]['name']==$result['name']){
+                    $totalPrice-=$_SESSION['cart'][$i]['price'];
+                    $_SESSION['cart'][$i]['quantity']=$quantity;
+                    $_SESSION['cart'][$i]['price']=$quantity* $pro[0]["price"];
+                    $totalPrice+=$_SESSION['cart'][$i]['price'];
+                    $result = [
+                        'name' => $pro[0]["name"],
+                        'price' => $_SESSION['cart'][$i]['price'],
+                        'quantity' =>$_SESSION['cart'][$i]['quantity'],
+                        'id' =>$pro[0]["id"],
+                        'first' => 0,
+                        'totalPrice' =>$totalPrice,
+                        'picture' => base64_encode($pro[0]["picture"]) // Encode the picture as base64
+                    ];
+                    echo json_encode($result);
+                    return;
+                }
+                $i++;
+              }
+             
+            // Add the item to the cart
+            $item = array(
+            'name' => $pro[0]["name"],
+            'price' => $pro[0]["price"]*$quantity,
+            'quantity' => $quantity,
+            'id' =>$pro[0]["id"],
+            'first' => 1,
+            'picture' => base64_encode($pro[0]["picture"]) // Encode the picture as base64
+            );
+            $_SESSION['cart'][] = $item;
+            $data['success'] = "Product Have Been saved";
+            $totalPrice+=$pro[0]["price"]*$quantity;
+            $result = [
+                'name' => $pro[0]["name"],
+                'price' => $_SESSION['cart'][$i]['price'],
+                'quantity' =>$_SESSION['cart'][$i]['quantity'],
+                'id' =>$pro[0]["id"],
+                'first' => 1,
+                'totalPrice' =>$totalPrice,
+                'picture' => base64_encode($pro[0]["picture"]) // Encode the picture as base64
             ];
-
             echo json_encode($result);
-     
-        } catch (Exception $e) {
-            $data['error'] = "'Caught exception: ',   $e->getMessage(), '\n'";    
+            
+        }
+        else 
+        {
+            $data['error'] = "Error";
         }
     }
 
-    public function delectfromcart($id)
+    // public function addToCard($id)
+    // {
+
+    //     $Product = $this->product->getProduct($id);
+       
+
+    //     session_start();
+    //     if (!isset($_SESSION['card'])) {
+    //         $_SESSION['card'] = array();
+    //     }
+
+
+    //     for ($i = 0; $i < count($_SESSION['card']); $i++) {
+    //         if ($Product[0]["name"] == $_SESSION['card'][$i]['name']) {
+    //             $_SESSION['card'][$i]['quantity'] += 1;
+    //             $_SESSION['card'][$i] = [
+    //                 'name' => $Product[0]["name"],
+    //                 'price' => $Product[0]["price"] * $_SESSION['cart'][$i]['quantity'],
+    //                 'quantity' => $_SESSION['cart'][$i]['quantity'],
+    //                 'id' => $Product[0]["id"]
+    //             ];
+    //             echo json_encode($_SESSION['card'][$i]);
+    //             return;
+    //         }
+    //     }
+
+    //     try {
+    //         $item = [
+    //             "name" => $Product[0]["name"],
+    //             "price" => $Product[0]["price"],
+    //             "quantity" => 1
+    //         ];
+    //         $_SESSION['card'] =  $item;
+    //         $data['success'] = "Product Have Been saved";
+    //         $result = [
+    //             'name' => $Product[0]["name"],
+    //             'price' => $Product[0]["price"],
+    //             'quantity' => 1,
+    //             'id' =>$Product[0]["id"],
+            
+    //         ];
+
+    //         echo json_encode($result);
+     
+    //     } catch (Exception $e) {
+    //         $data['error'] = "'Caught exception: ',   $e->getMessage(), '\n'";    
+    //     }
+    // }
+
+    
+
+
+    public function delectfromcart($id,$total)
     {
+        $totalPrice=$total;
         $pro=$this->product->getProduct($id);
         if($pro)
         {
             session_start();
-
-            // Check if the cart exists in the session
-
             if (isset($_SESSION['cart'])) {
-            $i=0;
-            foreach ($_SESSION['cart'] as $result){
+           
+            foreach ($_SESSION['cart'] as $i => $result){
                 if($pro[0]['name']==$result['name']){
+                    $totalPrice-=$_SESSION['cart'][$i]['price'];
                     unset($_SESSION['cart'][$i]);
                     $result = [
                         'name' => $result["name"],
                         'price' => $result["price"],
-                        'quantity' =>$result['quantity']
+                        'totalPrice' =>$totalPrice,
+                        'quantity' =>$result['quantity'],
+                        'i'=>$_SESSION['cart']
                     ];
-                    // var_dump($result);
                     echo json_encode($result);
                 }  
-                $i++;
               }
             }
         }
+        
         else 
         {
             $data['error'] = "Error";
--- App/Views/products/index.php
+++ App/Views/products/index.php
@@ -1,18 +1,20 @@
-<?php  include(VIEWS.'template'.DS.'header.php');
-    echo "<script src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js' integrity='sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p' crossorigin='anonymous'></script>
-    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js' integrity='sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF' crossorigin='anonymous'></script>
-    ";
-?>
-
+<?php  include(VIEWS.'template'.DS.'header.php');?>
+<!-- <script src="<?=BURL.'js/home.js'?>"></script> -->
+  <?php if(isset($success)): ?>
+                <h3 class="alert alert-success text-center"><?php  echo $success; ?></h3>
+            <?php endif; ?>
+            <?php if(isset($error)): ?>
+                <h3 class="alert alert-danger text-center"><?php  echo $error; ?></h3>
+            <?php endif; ?>
 <div class="container">
-<div class="row">
-
-  
-        <div class="row">
+  <div class="row">
+    <div class="col-md-8">
+      <h1>My Cafeteria Menu</h1>
+    </div>
+    <div class="col-md-4">
             <?php foreach($user as $row):  ?>
-                <div class="col-md-5 m-3">
-                    <div class="card p-4 rounded-5  border-dark" style=" box-shadow: 0px 0px 10px 0px rgb(0 0 0 / 18%);
-                    ">
+                <div class="col-md-12">
+                    <div class="card p-4 rounded-5  border-dark" >
                         <div class="row">
                             <div class="col-md-5 mt-3"  >
                             <img src="<?='data:image/jpeg;base64,'.base64_encode($row['picture'])?>"   width="100px" />
@@ -23,89 +25,257 @@
                     </div>
             </div>  
         </div>
-        </div>
-        
-        
+        </div>     
     <?php  endforeach; ?>
-    </div>
-</div>
-
-  <div class="row">
-
-  
-    <div class="col-md-12">
-      <h1>latest Order</h1>
     </div>
   </div>
   
   <div class="row">
     <div class="col-md-8 ">
-    <h1>My Cafeteria Menu</h1>
         <div class="row">
+        <?php $i=0; ?>
     <?php foreach($products as $row):  ?>
+     
         <div class="col-md-5 m-3">
             <div class="card p-4 rounded-5  border-dark" style="height:200px; box-shadow: 0px 0px 10px 0px rgb(0 0 0 / 18%);
             ">
                 <div class="row">
                      <div class="col-md-5 mt-3"  >
                      <img src="<?='data:image/jpeg;base64,'.base64_encode($row['picture'])?>"  height="50%" width="100px" />
-                     <a  class="btn rounded-circle  border-dark mt-2 font-weight-bold" >-</a>
-                     <span>1</span>
-                     <a  class="btn rounded-circle  border-dark mt-2 font-weight-bold" >+</a>
+                     <a  class="btn rounded-circle  border-dark mt-2 font-weight-bold" onclick="fundecreaseQuantity('localQuantity<?=$row['name']?>')" id="decreaseQuantity<?=$row["name"]?>" >-</a>
+                     <span id="localQuantity<?=$row["name"]?>">1</span>
+                     <a  id="increaseQuantity<?=$row["name"]?>" onclick="funincreaseQuantity('localQuantity<?=$row['name']?>')" class="btn rounded-circle  border-dark mt-2 font-weight-bold" >+</a>
 
                      </div>
                      <div class="col-md-7">
                             <div class="card-body">
-                        <h4 class="card-title" style="color: rgb(112, 66, 50)"> <?php echo $row['name']?></h4>
-                        <p class="card-text " >Price:<span class="font-weight-bold" style="color:orange" ><?php echo $row["price"] ?></span>$</p>
+                        <h4 class="card-title" style="color: rgb(112, 66, 50)"><?php echo $row['name']?></h4>
+                        <p class="card-text " >Price:<span id="localPrice" class="font-weight-bold" style="color:orange" ><?php echo $row["price"] ?></span>$</p>
 
-                        <button href="#"  onClick="addToCard(<?= $row['id'];?>)"
-                         class="btn rounded-pill"style="color:white;background-color:orange">Added To Cart</button>
-                         
-                     </div>
+                       <button onclick="openOrderDetails('<?php echo $row['id'] ?>','<?php echo $row['name'] ?>')" id="addCart"   class="btn rounded-pill"style="color:white;background-color:orange" >Add To Cart</button>
+
+                      </div>
                 </div>
             </div>
             
            
         </div>
         </div>
-        
+        <?php  $i++; ?>
         
     <?php  endforeach; ?>
     </div>
         
     
     </div>
-    <div class="col-md-4" style="background-color:white;height: auto;">
-        <H1 class="m-2" style="color: rgb(112, 66, 50)">Cart</H1>
-        <div class="card border border-white ">
+    
+        <div id="cart-form" class="col-md-4" style="background-color:white;height: auto;">
+            <H1 id="cartOrder" class="m-2" style="color: rgb(112, 66, 50)">Cart</H1>
+            <form action="<?php url('orders/store'); ?>" method="POST" >
+                <h3 style="color:orange">Notes</h3>
+                <textarea name="notes" id="notes" cols="30" rows="5"></textarea><br>
+                <input type="number" name="id_user" id="id_user" hidden>
+                <input type="number" name="totalPriceOrder" id="totalPriceOrder" hidden>
+                <input type="number" name="userroomNumber" id="userroomNumber" hidden>
+                
+                <div class="btn-group">
+        <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" id="userroom" data-bs-toggle="dropdown" aria-expanded="false">
+        Room
+        </button>
+        <ul class="dropdown-menu dropdown-menu-dark">
+                    <?php foreach($rooms as $row): ?>
+                      <li ><a class="dropdown-item" href="#" onclick="selectroom(this)"><?php echo $row['roomNumber']; ?></a></li>
+                    <?php endforeach; ?>
+        </ul>
+    </div>
                 <div class="row">
+                    <div class="col-md-8 p-3">Total Price</div>
+                    <div  class="col-md-4 p-3" style="color:orange"><span id="totalPrice">0.00</span>$</div>
+                </div>
+                <button type="submit" class="btn rounded-pill mb-3"style="background-color:orange">Place an Order</button>
+            </form>
+        
+        </div>
+        </div>
+
+    </div>
+<script>
+function openOrderDetails(id,nameProduct) {
+  let localQuantityelement = document.getElementById("localQuantity"+nameProduct);
+  let localQuantity = parseInt(localQuantityelement.innerText);
+
+  let totalprice=document.getElementById("totalPrice") ;
+  let  total=parseInt(totalprice.innerText)
+    fetch(`<?php url('/products/cart/${id}/${localQuantity}/${total}') ?>`)
+    .then(async (res) => {
+         
+    let data = await res.json();
+    console.log(data);
+    if(data["first"]==0){
+        let input = document.getElementById("quantity"+data["name"]);
+        let inputprice = document.getElementById("price"+data["name"]);
+
+        if(input){
+            input.innerText = parseInt(data["quantity"]) ;
+            inputprice.innerText = parseInt(data["price"]);
+        }
+    }
+    else{
+        let row = document.createElement("div");
+        row.id="id"+data['name']
+        row.className="card border border-white";
+        const imageUrl = `data:image/jpeg;base64,${data['picture']}`;
+        row.innerHTML=`
+        <div class="row">
                      <div class="col-md-5 mt-3"  >
-                     <img src="<?='data:image/jpeg;base64,'.base64_encode($row['picture'])?>"  height="100%" width="100px" />
-                     
+                     <img id='image${data['name']}' src="${imageUrl}"  height="100%" width="100px" />
+                      
                      </div>
                      <div class="col-md-7">
                             <div class="card-body">
-                        <h4 class="card-title" style="color: rgb(112, 66, 50)"><?php echo $row['name']?></h4>
-                        <p class="card-text " >Price:<span class="font-weight-bold" style="color:orange" ><?php echo $row["price"] ?></span>$</p>
-                        <a  class="btn rounded-circle  border-dark mt-2 font-weight-bold" >-</a>
-                        <span>1</span>
-                        <a  class="btn rounded-circle  border-dark mt-2 font-weight-bold" >+</a>
-
+                        <h4 class="card-title" id="${data['name']}" style="color: rgb(112, 66, 50)">${data['name']}</h4>
+                        <span  class="card-text " >Price:<span id='price${data['name']}' class="font-weight-bold" style="color:orange" >${data['price']}</span>$</span>
+                        <a  onclick="decreaseQuantity(${data['id']},'quantity${data['name']}')" class="btn rounded-circle ms-3 border-dark mt-2 font-weight-bold" >-</a>
+                        <span id='quantity${data['name']}'>${data['quantity']}</span>
+                        <a  onclick="increaseQuantity(${data['id']},'quantity${data['name']}')" class="btn rounded-circle  border-dark mt-2 font-weight-bold" >+</a>
+                        <button onclick="deleteCart(${data['id']},this)"  class="btn rounded-pill ms-5 mt-3"style="color:white;background-color:red">Delete</button>
+                        
                         </div>
                 </div>
-        </div>
-        <hr>
+        `;
+        let exitelement = document.getElementById("cartOrder") ;
+        // document.getElementById("cart-form").appendChild(row);//to show on myView
+        exitelement.insertAdjacentElement("afterend", row);
+        const newElement = document.createElement("hr");
+        newElement.id="hr"+data["name"];
+        row.insertAdjacentElement("afterend", newElement);
+        
+    }    
+    let totalprice=document.getElementById("totalPrice") ;
+        totalprice.innerText=data["totalPrice"];
+        
+    let totalPriceOrder=document.getElementById("totalPriceOrder") ;
+    totalPriceOrder.value=data["totalPrice"];
        
-        <h3 style="color:orange">Notes</h3>
-        <textarea name="notes" id="notes" cols="30" rows="5"></textarea>
-        <div class="row">
-            <div class="col-md-8 p-3">Total Price</div>
-            <div class="col-md-4 p-3" style="color:orange">5.00$</div>
-        </div>
-        <button type="button" class="btn rounded-pill mb-3"style="background-color:orange">Place an Order</button>
-    </div>
-  </div>
-</div>
+    })
+    .catch((error) => console.log(error));
 
-<?php  include(VIEWS.'template'.DS.'footer.php'); ?>
+}
+ //when click button delete 
+ function deleteCart(id,btn){
+    let totalprice=document.getElementById("totalPrice") ;
+  let  total=parseInt(totalprice.innerText);
+
+  console.log(id,total);
+  
+    fetch(`<?php url('/products/delectfromcart/${id}/${total}') ?>`)
+    .then(async (res) => {
+        
+        let data = await res.json(); 
+        let totalprice=document.getElementById("totalPrice") ;
+        totalprice.innerText=data["totalPrice"];
+        let totalPriceOrder=document.getElementById("totalPriceOrder") ;
+    totalPriceOrder.value=data["totalPrice"];
+        const rowid = document.getElementById("id"+data["name"]);
+        rowid.remove();
+        const rowhr = document.getElementById("hr"+data["name"]);
+        rowhr.remove();
+        
+        
+    })
+    .catch((error) => console.log(error));
+   }
+function funincreaseQuantity(name){
+    let quantity = document.getElementById(name);
+    quantity.innerText=parseInt(quantity.innerText)+1;
+
+}
+function increaseQuantity(id,name){
+    let quantity = document.getElementById(name);
+    quantity.innerText=parseInt(quantity.innerText)+1;
+  let localQuantity = parseInt(quantity.innerText);
+
+  let totalprice=document.getElementById("totalPrice") ;
+  let  total=parseInt(totalprice.innerText)
+
+  
+    fetch(`<?php url('/products/cart/${id}/${localQuantity}/${total}') ?>`)
+    .then(async (res) => {
+         
+    let data = await res.json();
+    let totalPriceOrder=document.getElementById("totalPriceOrder") ;
+    totalPriceOrder.value=data["totalPrice"];
+    if(data["first"]==0){
+        let input = document.getElementById("quantity"+data["name"]);
+        let inputprice = document.getElementById("price"+data["name"]);
+
+        if(input){
+            input.innerText = parseInt(data["quantity"]) ;
+            inputprice.innerText = parseInt(data["price"]);
+            let totalprice=document.getElementById("totalPrice") ;
+        totalprice.innerText=data["totalPrice"];
+        }
+    }
+})
+    .catch((error) => console.log(error));
+}
+function fundecreaseQuantity(name){
+    let quantity = document.getElementById(name);
+    if(parseInt(quantity.innerText)-1>0)
+          quantity.innerText=parseInt(quantity.innerText)-1;
+
+}
+function decreaseQuantity(id,name){
+    let quantity = document.getElementById(name);
+    if(parseInt(quantity.innerText)-1>0)
+          quantity.innerText=parseInt(quantity.innerText)-1;
+  let localQuantity = parseInt(quantity.innerText);
+
+  let totalprice=document.getElementById("totalPrice") ;
+  let  total=parseInt(totalprice.innerText)
+  
+    fetch(`<?php url('/products/cart/${id}/${localQuantity}/${total}') ?>`)
+    .then(async (res) => {
+         
+    let data = await res.json();
+    let totalPriceOrder=document.getElementById("totalPriceOrder") ;
+    totalPriceOrder.value=data["totalPrice"];
+    if(data["first"]==0){
+        let input = document.getElementById("quantity"+data["name"]);
+        let inputprice = document.getElementById("price"+data["name"]);
+
+        if(input){
+            input.innerText = parseInt(data["quantity"]) ;
+            inputprice.innerText = parseInt(data["price"]);
+            let totalprice=document.getElementById("totalPrice") ;
+        totalprice.innerText=data["totalPrice"];
+        }
+    }
+})
+    .catch((error) => console.log(error));
+}
+function selectUser(element,id) {
+    var selectedUser = element.textContent;
+    var dropdownButton = document.querySelector('.dropdown-toggle');
+    dropdownButton.textContent = selectedUser;
+    // console.log(id)
+
+    fetch(`<?php url('/products/getIdUser/${id}') ?>`)
+        .then(async (res) => {
+            let data = await res.json();
+            
+            let id = document.getElementById("id_user");
+            id.value=data['userId'];
+        })
+}
+function selectroom(element){
+    var selectedUser = element.textContent;
+    var dropdownButton = document.querySelector('#userroom');
+    dropdownButton.textContent = selectedUser;
+    
+    let id = document.getElementById("userroomNumber");
+            id.value=selectedUser;
+}
+</script>
+
+<?php  include(VIEWS.'template'.DS.'footer.php');  ?>
